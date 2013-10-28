233d3fe99de73b082fca7bdfcf18129446b72fc0
Pelle Bjerkestrand <pelle@pellebjerkestrand.net>
2013-10-28 18:39:50 +0100

    Debounce added
    Thanks, Remy
    (http://remysharp.com/2010/07/21/throttling-function-calls/)

--- jquery.skrulle.js
+++ jquery.skrulle.js
@@ -1,3 +1,10 @@
+/**
+ *  jQuery Skrulle
+ *
+ *  By Pelle Bjerkestrand - http://pellebjerkestrand.net/
+ *  License: UNLICENSE - http://unlicense.org/
+ */
+
 ;(function($){
     'use strict';
 
@@ -26,29 +33,43 @@
             settings: {},
             previousScrollTop: 0,
             element: null,
+            debounce: function(fn, delay) {
+                // http://remysharp.com/2010/07/21/throttling-function-calls/
+                var timer = null;
+                return function () {
+                    var context = this, args = arguments;
+                    clearTimeout(timer);
+                    timer = setTimeout(function () {
+                        fn.apply(context, args);
+                    }, delay);
+                };
+            },
+            handleScroll: function(oldY, newY){
+                if(oldY < newY){
+                    // scrolling down
+                    skrulle.element
+                        .removeClass(skrulle.settings.up)
+                        .addClass(skrulle.settings.down);
+                } else if (oldY > newY){
+                    // scrolling up
+                    skrulle.element
+                        .removeClass(skrulle.settings.down)
+                        .addClass(skrulle.settings.up);
+                }
+
+                skrulle.previousScrollTop = newY;
+            },
             init: function(){
                 $.extend(skrulle.settings, defaults, options);
 
-                $(window).on('scroll', function(){
-                    if(!skrulle.element){
-                        skrulle.element = $(skrulle.settings.selector);
-                        skrulle.previousScrollTop = $this.scrollTop();
-                    }
-
-                    if(skrulle.previousScrollTop < $this.scrollTop()){
-                        // scrolling down
-                        skrulle.element
-                            .removeClass(skrulle.settings.up)
-                            .addClass(skrulle.settings.down);
-                    } else {
-                        // scrolling up
-                        skrulle.element
-                            .removeClass(skrulle.settings.down)
-                            .addClass(skrulle.settings.up);
-                    }
-
+                if(!skrulle.element){
+                    skrulle.element = $(skrulle.settings.selector);
                     skrulle.previousScrollTop = $this.scrollTop();
-                });
+                }
+
+                $(window).on('scroll', skrulle.debounce(function(){
+                    skrulle.handleScroll(skrulle.previousScrollTop, $this.scrollTop());
+                }, 150));
             }
         };
 
--- skrulle.js
+++ skrulle.js
@@ -1,3 +1,10 @@
+/**
+ *  Skrulle
+ *
+ *  By Pelle Bjerkestrand - http://pellebjerkestrand.net/
+ *  License: UNLICENSE - http://unlicense.org/
+ */
+
 ;(function(window, document){
     'use strict';
 
@@ -5,20 +12,35 @@
         up: 'up',
         down: 'down',
         previousScrollY: 0,
-        init: function(){
-            window.addEventListener('scroll', function(){
-                if(skrulle.previousScrollY < window.scrollY){
-                    // scrolling down
-                    document.documentElement.classList.remove(skrulle.up);
-                    document.documentElement.classList.add(skrulle.down);
-                } else {
-                    // scrolling up
-                    document.documentElement.classList.remove(skrulle.down);
-                    document.documentElement.classList.add(skrulle.up);
-                }
+        debounce: function(fn, delay) {
+            // http://remysharp.com/2010/07/21/throttling-function-calls/
+            var timer = null;
+            return function () {
+                var context = this, args = arguments;
+                clearTimeout(timer);
+                timer = setTimeout(function () {
+                    fn.apply(context, args);
+                }, delay);
+            };
+        },
+        handleScroll: function(oldY, newY){
+            if(oldY < newY){
+                // scrolling down
+                document.documentElement.classList.remove(skrulle.up);
+                document.documentElement.classList.add(skrulle.down);
+            } else if(oldY > newY) {
+                // scrolling up
+                document.documentElement.classList.remove(skrulle.down);
+                document.documentElement.classList.add(skrulle.up);
+            }
+            // equal values is a no op
 
-                skrulle.previousScrollY = window.scrollY;
-            });
+            skrulle.previousScrollY = newY;
+        },
+        init: function(){
+            window.addEventListener('scroll', skrulle.debounce(function(){
+                skrulle.handleScroll(skrulle.previousScrollY, window.scrollY);
+            }, 150));
         }
     };
 
